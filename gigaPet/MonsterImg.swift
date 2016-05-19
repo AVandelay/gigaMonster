2da62655e1074381f95a020395f792a472b91203
westdorp <westdorp@gmail.com>
2016-05-19 10:33:57 -0700

    animating lost lives

---
+++ gigaPet/MonsterImg.swift
@@ -0,0 +1,57 @@
+//
+//  MonsterImg.swift
+//  gigaPet
+//
+//  Created by kentaro on 5/19/16.
+//  Copyright © 2016 ken. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MonsterImg: UIImageView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        playIdleAnimation()
+    }
+    
+    func playIdleAnimation() {
+        
+        self.image = UIImage(named: "idle1.png")
+        
+        self.animationImages = nil
+        
+        var imgArray = [UIImage]()
+        for index in 1...4 {
+            let img = UIImage(named:"idle\(index).png")
+            imgArray.append(img!)
+        }
+        
+        self.animationImages = imgArray
+        self.animationDuration = 0.8
+        self.animationRepeatCount = 0
+        self.startAnimating()
+    }
+    
+    func playDeathAnimation() {
+        
+        self.image = UIImage(named: "dead5.png")
+        
+        self.animationImages = nil
+        
+        var imgArray = [UIImage]()
+        for index in 1...5 {
+            let img = UIImage(named:"dead\(index).png")
+            imgArray.append(img!)
+        }
+        
+        self.animationImages = imgArray
+        self.animationDuration = 0.8
+        self.animationRepeatCount = 1
+        self.startAnimating()
+    }
+}
